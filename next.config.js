aac6583ab30481fe27002e0ee30a25c2e48c8c45
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2024-07-17 22:17:28 +0900

    実装途中

--- next.config.js
+++ next.config.js
@@ -1,23 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = (() => {
-    let compilerConfig = {
-      // styledComponentsの有効化
-      styledComponents: true,
+  let compilerConfig = {
+    // styledComponentsの有効化
+    styledComponents: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    compilerConfig = {
+      ...compilerConfig,
+      // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+      reactRemoveProperties: { properties: ['^data-testid$'] },
     };
-  
-    if (process.env.NODE_ENV === 'production') {
-      compilerConfig = {
-        ...compilerConfig,
-        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
-        reactRemoveProperties: { properties: ['^data-testid$'] },
-      };
-    }
-  
-    return {
-      reactStrictMode: true,
-      compiler: compilerConfig,
-    };
-  })();
-  
-  module.exports = nextConfig;
-  
+  }
+
+  return {
+    reactStrictMode: true,
+    compiler: compilerConfig,
+    async rewrites() {
+      return [
+        {
+          // 例: /api/proxy
+          source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+          // 例: http://localhost:8000
+          destination: `${process.env.API_BASE_URL}/:match*`,
+        },
+      ];
+    },
+  };
+})();
+
+module.exports = nextConfig;
